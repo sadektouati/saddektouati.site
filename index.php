ec44d75e857c002d048d832400532f7916b42118
sadektouati <bilal2dellys@hotmail.com>
2023-05-13 07:37:03 -0400

    styling

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style.css?<?= filemtime('style.css')?>">
     <title>Saddek touati</title>
 </head>
 <body>
@@ -14,20 +14,83 @@
         in this page, some links might not work properly
     </span>
 </div>
-<h1>Touati Saddek | Full stack developer | with an eye on UI/UX | and some security :)</h1>
+
+<header>
+    <h1>
+        Touati Saddek | Full stack developer
+    </h1>
+    <span>
+        I have an eye on UI/UX | and some security :)
+    </span>
+</header>
+
+<section>
+
+    <div>
+        <h3>PHP</h3>
+        <p>
+            Je suis un développeur web passionné, spécialisé dans le développement PHP avec le framework Laravel. Avec une solide expérience dans la création de sites web dynamiques et interactifs, je suis constamment à la recherche de nouvelles façons de repousser les limites de la technologie.
+        </p>
+    </div>
+
+    <div>
+        <h3>SQL</h3>
+        <p>
+            Mon expertise s'étend aux bases de données MySQL et PostgreSQL, me permettant de gérer efficacement les données et de créer des applications robustes. Ma connaissance approfondie de SQL me permet d'optimiser les performances des requêtes et d'assurer une gestion efficace des données.
+        </p>
+    </div>
+
+    <div>
+        <h3>CSS</h3>
+        <p>
+            En ce qui concerne le front-end, je suis compétent en CSS et JavaScript, et j'ai développé une expertise particulière en Vue.js, un framework JavaScript progressif qui offre une expérience utilisateur fluide et réactive.
+        </p>
+    </div>
+
+    <div>
+        <h3>UX/UI</h3>
+        <p>
+            En tant que développeur web, je comprends l'importance de l'interface utilisateur (UI) et de l'expérience utilisateur (UX). Je m'efforce de créer des interfaces intuitives, esthétiques et conviviales pour offrir aux utilisateurs une expérience agréable et engageante.
+        </p>
+    </div>
+
+    <div>
+        <h3>opportunités</h3>
+        <p>
+            Je suis toujours à l'affût des dernières tendances et technologies de l'industrie, ce qui me permet de rester à jour et de proposer des solutions modernes et innovantes à mes clients.
+        </p>
+    </div>
+
+    <div>
+        <h3>Invitation</h3>
+        <p>
+            Explorez mon portfolio pour découvrir les projets sur lesquels j'ai travaillé et pour en savoir plus sur mes compétences et mes réalisations. N'hésitez pas à me contacter si vous avez des questions ou si vous souhaitez discuter de votre prochain projet. Je serai ravi de collaborer avec vous pour concrétiser votre vision.
+        </p>
+    </div>
+
+    <div>
+        <p>
+        Merci de votre visite et j'espère avoir l'occasion de travailler avec vous bientôt !
+        </p>
+    </div>
+
+</section>
+
 <?php include('school-projects.php'); ?>
-    <main>
-    <h2>School</h2>
+<main>
     <?php foreach ($schoolProjects as $subcategory => $value) { ?>
         <section>
-            <h3><?= $subcategory ?></h3>
+            <h2><?= $subcategory ?> <sub>@school</sub></h2>
             <?php foreach ($value as $title => $works) { ?>
                 <article>
-                    <h4><?= $title ?></h4>
-                    <div>
+                    <h3>
+                        <sup><?= count($works) ?> projet<?= count($works)>1?'s':'' ?> dans</sup>
+                        <?= $title ?>
+                    </h3>
+                    <div class="scrollable">
                         <?php foreach ($works as $work) { ?>
                             <div>
-                                <?= $work[2]??"this is an explanation" ?>
+                                <p><?= $work[2]??"this is an explanation" ?></p>
                                 <a href="<?= $work[0] ?>" target="_blank"><?= $work[1] ?></a>
                             </div>
                         <?php } ?>
--- school-projects.php
+++ school-projects.php
@@ -1,55 +1,5 @@
 <?php
 $schoolProjects = [
-    'Session 1' => [
-        "Initiation a la création de sites web (HTML, CSS)" => [
-            ["/schl/s1-c-s-w/portfolio-1", "Portfolio 1"],
-            ["/schl/s1-c-s-w/portfolio-2", "Portfolio 2"],
-        ],
-        "Introduction à la programmation (PHP)" => [
-            ["/schl/s1-prgrmtn/affichage-notes", "Affichage Notes"],
-        ]
-    ],
-    
-    'Session 2' => [
-        "Création et désign site web (Désign UI et CSS)" => [
-            ["/schl/s2-crtn-dsgn-st-wb/tp-1", "TP 1"],
-            ["/schl/s2-crtn-dsgn-st-wb/tp-2", "TP 2"],
-            ["/schl/s2-crtn-dsgn-st-wb/tp-3", "TP 3"],
-            ["/schl/s2-crtn-dsgn-st-wb/epreuve-final/materiel", "Epreuve Final"],
-        ],
-    "Javascript" => [
-            ["/schl/s2-js/tp-1", "TP 1"],
-            ["/schl/s2-js/tp-2", "TP 2"],
-        ],
-    "Programmation web (PHP, SQL)" => [
-            ["/schl/s2-prgrmtn/blue-bonnet", "Blue Bonnet"],
-            ["/schl/s2-prgrmtn/critiques-films-mvc", "Critiques Film"],
-            ["/schl/s2-prgrmtn/site-critiques-films", "Site Critiques Film"],
-            ["/schl/s2-prgrmtn/introduction-mvc", "Introduction MVC"],
-            ["/schl/s2-prgrmtn/questionnaire-session", "Questionnaire Session"],
-        ]
-    ],
-    
-    'Session 3' => [
-        "Programmation web Avancée (PHP, SQL, MVC)" => [
-            ["/schl/s3-prgrmtn-w-a/exercice-1", "Exercice 1"],
-            ["/schl/s3-prgrmtn-w-a/exercice-2", "Exercice 2"],
-            ["/schl/s3-prgrmtn-w-a/tp-1", "Image de Jour"],
-            ["/schl/s3-prgrmtn-w-a/tp-2", "Image de Jour Improved"],
-            ["/schl/s3-prgrmtn-w-a/examen-final", "Examen Final"],
-        ],
-        "UI/UX appliquée au site web" => [
-            ["/schl/s3-ux-ui/1-analyse-du-devis", "Analyse de Devis"],
-            ["/schl/s3-ux-ui/2-strategie-ux", "Stratégie UX"],
-            ["/schl/s3-ux-ui/3-exploration-artistique", "Exploration Artistique"],
-            ["/schl/s3-ux-ui/4-langage-graphique", "Langage Graphique"],
-            ["/schl/s3-ux-ui/5-catalogue-de-produits", "Catalogue de Produits"],
-            ["/schl/s3-ux-ui/6-fiche-produit", "Fiche Produit"],
-            ["/schl/s3-ux-ui/7-vitrine-d-accueil", "Vitrine d'Accueil"],
-            ["/schl/s3-ux-ui/8-examen-finale", "Examen Final"],
-        ],
-    ],
-    
     'Session 4' => [    
         "Cadriciel (Laravel)" => [
             ["/schl/s4-laravel/exercice-1", "Exercice 1"],
@@ -69,5 +19,55 @@ $schoolProjects = [
             ["/schl/s4-t-a-p/tp-2/public", "TP 2"],
             ["/schl/s4-t-a-p/tp-3/client/public", "TP 2"],
         ],
+    ],
+
+    'Session 3' => [
+        "Programmation web Avancée (PHP, SQL, MVC)" => [
+            ["/schl/s3-prgrmtn-w-a/exercice-1", "Exercice 1"],
+            ["/schl/s3-prgrmtn-w-a/exercice-2", "Exercice 2"],
+            ["/schl/s3-prgrmtn-w-a/tp-1", "Image de Jour"],
+            ["/schl/s3-prgrmtn-w-a/tp-2", "Image de Jour Improved"],
+            ["/schl/s3-prgrmtn-w-a/examen-final", "Examen Final"],
+        ],
+        "UI/UX appliquée au site web" => [
+            ["/schl/s3-ux-ui/1-analyse-du-devis", "Analyse de Devis"],
+            ["/schl/s3-ux-ui/2-strategie-ux", "Stratégie UX"],
+            ["/schl/s3-ux-ui/3-exploration-artistique", "Exploration Artistique"],
+            ["/schl/s3-ux-ui/4-langage-graphique", "Langage Graphique"],
+            ["/schl/s3-ux-ui/5-catalogue-de-produits", "Catalogue de Produits"],
+            ["/schl/s3-ux-ui/6-fiche-produit", "Fiche Produit"],
+            ["/schl/s3-ux-ui/7-vitrine-d-accueil", "Vitrine d'Accueil"],
+            ["/schl/s3-ux-ui/8-examen-finale", "Examen Final"],
+        ],
+    ],
+
+    'Session 2' => [
+        "Création et désign site web (Désign UI et CSS)" => [
+            ["/schl/s2-crtn-dsgn-st-wb/tp-1", "TP 1"],
+            ["/schl/s2-crtn-dsgn-st-wb/tp-2", "TP 2"],
+            ["/schl/s2-crtn-dsgn-st-wb/tp-3", "TP 3"],
+            ["/schl/s2-crtn-dsgn-st-wb/epreuve-final/materiel", "Epreuve Final"],
+        ],
+    "Javascript" => [
+            ["/schl/s2-js/tp-1", "TP 1"],
+            ["/schl/s2-js/tp-2", "TP 2"],
+        ],
+    "Programmation web (PHP, SQL)" => [
+            ["/schl/s2-prgrmtn/blue-bonnet", "Blue Bonnet"],
+            ["/schl/s2-prgrmtn/critiques-films-mvc", "Critiques Film"],
+            ["/schl/s2-prgrmtn/site-critiques-films", "Site Critiques Film"],
+            ["/schl/s2-prgrmtn/introduction-mvc", "Introduction MVC"],
+            ["/schl/s2-prgrmtn/questionnaire-session", "Questionnaire Session"],
+        ]
+    ],
+
+    'Session 1' => [
+        "Initiation a la création de sites web (HTML, CSS)" => [
+            ["/schl/s1-c-s-w/portfolio-1", "Portfolio 1"],
+            ["/schl/s1-c-s-w/portfolio-2", "Portfolio 2"],
+        ],
+        "Introduction à la programmation (PHP)" => [
+            ["/schl/s1-prgrmtn/affichage-notes", "Affichage Notes"],
+        ]
     ],
 ];
