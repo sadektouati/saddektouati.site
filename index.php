c0f7d351c5ff582a23537fcfaa7204dcfdcc5f55
sadektouati <bilal2dellys@hotmail.com>
2023-08-21 06:28:31 -0400

    amended image extension

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ include('school-projects.php');
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="/icones/favicon.png">
     <?php foreach ($projects as $key => $value) { ?>
-        <link rel="preload" as="image" href="/images/captures/<?= $key ?>-min.png" />
+        <link rel="preload" as="image" href="/images/captures/<?= $key ?>-min.webp" />
     <?php } ?>
 
     <link rel="stylesheet" href="style.css?<?= filemtime('style.css')?>">
